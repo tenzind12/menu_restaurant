4d94fd1883fa06f9f4549420a9a68ee5fd31ea98
Tenzin <dhontenzin@gmail.com>
2022-02-22 22:55:05 +0100

    Updates

---
+++ precache-manifest.d65c6a4b4e6689ad62382ea70354d695.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5145e46f910e23398db626de3c61cbb4",
+    "url": "/menu_restaurant/index.html"
+  },
+  {
+    "revision": "369473483289ed261fe6",
+    "url": "/menu_restaurant/static/css/main.26e112f5.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/menu_restaurant/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/menu_restaurant/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "369473483289ed261fe6",
+    "url": "/menu_restaurant/static/js/main.663133ef.chunk.js"
+  },
+  {
+    "revision": "6d3bd214c056206ab077",
+    "url": "/menu_restaurant/static/js/runtime-main.6820323a.js"
+  }
+]);
